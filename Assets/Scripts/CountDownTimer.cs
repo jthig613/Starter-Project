f63f9e73f35dac77b94d8303780a7a4fe1fc9cbd
jthig613 <67336532+jthig613@users.noreply.github.com>
2021-01-28 11:58:56 -0500

    Main resources for project.

---
+++ Assets/Scripts/CountDownTimer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountDownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 10f;
+    [SerializeField] Text countDownTimer;
+    AudioSource audioSource;
+    public AudioClip ThemeSong;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countDownTimer.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            SceneManager.LoadScene("Failure");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public Rigidbody2D rb;
+
+    float mx;
+    public Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mx = Input.GetAxisRaw("Horizontal");
+            
+    if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * 1000);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
+        rb.velocity = movement;
+
+        if (Mathf.Abs(mx) > 0.05)
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Lava")
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene("Failure");
+        }
+        if (collision.gameObject.tag == "Goal")
+        {
+            SceneManager.LoadScene("Victory");
+        }
+        if (collision.gameObject.tag == "Door")
+        {
+            SceneManager.LoadScene("Test Scene");
+        }
+        if (collision.gameObject.tag == "Death Wall") 
+        {
+            SceneManager.LoadScene("Test Scene");
+        }
+
+
+
+    }            
+}
